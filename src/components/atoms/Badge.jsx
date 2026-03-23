c824e399f53f396c29846e3cc97ac5e065a3c306
KenjiTECHinc <Achita2005.tech.inc@gmail.com>
2026-03-24 03:32:02 +0800

    fix contrast
    - fix contrast on badge to match WCAG

--- src/components/atoms/Badge.jsx
+++ src/components/atoms/Badge.jsx
@@ -1,7 +1,7 @@
 // src/components/atoms/Badge.jsx
 export function Badge({ text }) {
     return (
-        <span className="inline-flex items-center px-2.5 py-1 rounded-full text-xs font-normal bg-primary-100 text-primary-600">
+        <span className="inline-flex items-center px-2.5 py-1 rounded-full text-xs font-normal bg-primary-100 text-primary-650">
             {text}
         </span>
     );
